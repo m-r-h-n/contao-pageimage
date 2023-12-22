8762f3ee7cb744ad6e8885f6f0bda8f09eaf180d
m-r-h-n <am@mrhn.de>
2023-12-22 22:10:47 +0100

    Update PageimageController.php

--- src/Controller/PageimageController.php
+++ src/Controller/PageimageController.php
@@ -8,6 +8,7 @@ use Contao\Controller;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
 use Contao\Image;
+use Contao\CoreBundle\Image\Studio\Studio;
 use Contao\ModuleModel;
 use Contao\PageModel;
 use Contao\StringUtil;
@@ -15,6 +16,7 @@ use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Terminal42\PageimageBundle\PageimageHelper;
+use Terminal42\PageimageBundle\PagevideoHelper;
 
 /**
  * @FrontendModule(category="miscellaneous")
@@ -26,36 +28,65 @@ class PageimageController extends AbstractFrontendModuleController
      */
     private $helper;
 
-    public function __construct(PageimageHelper $helper)
+    /**
+     * @var PagevideoHelper
+     */
+    private $videohelper;
+    
+    private $studio;
+
+    public function __construct(PageimageHelper $helper, PagevideoHelper $videohelper, Studio $studio)
     {
         $this->helper = $helper;
+        $this->videohelper = $videohelper;
+        $this->studio = $studio;
     }
 
     protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
     {
         $images = $this->getImages($model);
+        $videos = $this->getVideos($model);
 
-        if (empty($images)) {
+        if (empty($images) && empty($videos)) {
             return new Response();
         }
 
+        //print_r($videos);
+
         $templateData = [];
+        $templateDataVideos = [];
+        
+        $figure = $this->studio->createFigureBuilder()->setSize($model->imgSize);
 
         foreach ($images as $image) {
-            $templateData[] = $this->generateImage($image, $model);
+            //$templateData[] = $this->generateImage($image, $model);
+            $templateData[] = $figure->from($image['path'])->build()->getLegacyTemplateData();
         }
 
+        foreach ($videos as $video) {
+            //$templateDataVideos[] = $this->generateVideo($video, $model);
+            $templateDataVideos[] = $video;
+        }
+        /*
         $size = StringUtil::deserialize($model->imgSize);
         $image['src'] = Image::get($image['path'], $size[0], $size[1], $size[2]);
-
         $template->setData(array_merge($template->getData(), $templateData[0]));
+        */
+        $template->setData(array_merge($template->getData(), $templateData[0]));
+        
         $template->allImages = $templateData;
+        $template->allVideos = $templateDataVideos;
 
+        //print_r($templateData);
         // Lazy-load the media queries
+        /*
         $template->mediaQueries = function () use ($templateData) {
             return $this->compileMediaQueries($templateData[0]['picture']);
         };
-
+        */
+        
+        $template->mediaQueries = fn () => $this->compileMediaQueries($templateData[0]['picture']);
+        
         return $template->getResponse();
     }
 
@@ -116,9 +147,9 @@ class PageimageController extends AbstractFrontendModuleController
 
         foreach ($sources as $value) {
             foreach (StringUtil::trimsplit(',', $value['srcset']) as $srcset) {
-                [$src, $density] = StringUtil::trimsplit(' ', $srcset);
+                [$src, $density] = StringUtil::trimsplit(' ', $srcset) + [null, null];
 
-                if (null === $density) {
+                if (null === $density || !str_ends_with((string) $density, 'x')) {
                     continue;
                 }
 
@@ -127,10 +158,10 @@ class PageimageController extends AbstractFrontendModuleController
                 if (1 !== (int) $density || !empty($value['media'])) {
                     $mediaQueries[] = [
                         'mq' => sprintf(
-                            $density > 1 ? 'screen and %1$s%2$s, screen and %1$s%3$s' : 'screen and %1$s',
-                            $value['media'] ?: '',
+                            $density > 1 ? 'screen %1$s%2$s, screen and %1$s%3$s' : 'screen and %1$s',
+                            $value['media'] ? " and {$value['media']}" : '',
                             $density > 1 ? " and (-webkit-min-device-pixel-ratio: $density)" : '',
-                            $density > 1 ? " and (min-resolution: {$density}dppx)" : ''
+                            $density > 1 ? " and (min-resolution: {$density}dppx)" : '',
                         ),
                         'src' => $src,
                     ];
@@ -140,4 +171,41 @@ class PageimageController extends AbstractFrontendModuleController
 
         return $mediaQueries;
     }
+
+    private function getVideos(ModuleModel $model): array
+    {
+        if ($model->defineRoot) {
+            $objPage = PageModel::findByPk($model->rootPage);
+        } else {
+            global $objPage;
+        }
+
+        if (null === $objPage) {
+            return [];
+        }
+
+        $videos = $this->videohelper->findForPage($objPage, (bool) $model->inheritPageImage);
+        if (null === $videos) {
+            return [];
+        }
+
+        if ($model->allPageImages) {
+            return $videos;
+        }
+
+        if ($model->randomPageImage) {
+            $index = random_int(0, \count($videos) - 1);
+        } else {
+            $index = $model->levelOffset;
+        }
+
+        if (!isset($videos[$index])) {
+            return [];
+        }
+
+        return [$videos[$index]];
+    }
+
+
+
 }
